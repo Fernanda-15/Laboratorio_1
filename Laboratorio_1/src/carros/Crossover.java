ff49d4d9321b113fc1ecfa9c0250fb62f046eeb0
Fernanda-15 <fernandaqp15@gmail.com>
2022-04-12 00:49:47 -0600

    Clase Crossover

---
+++ Laboratorio_1/src/carros/Crossover.java
@@ -0,0 +1,21 @@
+package carros;
+
+import motores.Motor;
+
+public class Crossover extends CarroBase{
+
+	public Crossover(Motor motor) {
+		super(motor);
+	}
+
+	@Override  
+	public void imprimirCategoria() {
+		System.out.println("Soy crossover");
+		imprimirMotor();
+	
+	}
+	
+	public void imprimirCategoria(String name) {
+	  System.out.println(name);
+	}
+}
